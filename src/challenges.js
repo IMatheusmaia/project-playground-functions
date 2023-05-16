47c70ca636f13d627a5a3704544be8281e356dda
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-05-16 16:39:17 -0300

    teste desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -80,6 +80,20 @@ function calcAllAreas(base, height, form){
 calcAllAreas();
 
 // Desafio 7 - Crie a função catAndMouse
+function catAndMouse(mouse, cat1, cat2){
+  let distance1= mouse - cat1;
+  let distance2= mouse - cat2;
+  
+    if(distance1 < distance2 && distance1 !== distance2){
+      return `cat1`
+    }else if(distance2 < distance1 && distance2 !== distance1){
+      return `cat2`
+    }else if(distance1 == distance2){
+      return `os gatos trombam e o rato foge`
+    }
+
+}
+console.log(catAndMouse(10, 8, 9));
 
 // Desafio 8 - Crie a função fizzBuzz
 
