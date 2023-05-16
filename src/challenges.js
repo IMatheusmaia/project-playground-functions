4edfbffeabd4d8a56db4b0175481621b154cbb87
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-05-16 19:28:01 -0300

    teste desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -40,39 +40,39 @@ footballPoints(14, 8);
 
 // Desafio 5 - Crie a função highestCount
 function highestCount(listNumbers) {
-  let highNumber= Math.max(...listNumbers);
-  let cont= 0;
-    for(let i=0; i<listNumbers.length; i++){
-      if(highNumber == listNumbers[i]){
-        cont++
-      }
+  let highNumber = Math.max(...listNumbers);
+  let cont = 0;
+  for (let i = 0; i < listNumbers.length; i++) {
+    if (highNumber == listNumbers[i]) {
+      cont++
     }
-      return cont
+  }
+  return cont
 }
 highestCount([0, 4, 4, 9, 9, 2, 1]);
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
-function calcTriangleArea(base, height){
-  let triangleArea = (base*height)/2;
-    return triangleArea;
+function calcTriangleArea(base, height) {
+  let triangleArea = (base * height) / 2;
+  return triangleArea;
 }
 calcTriangleArea();
 
-function calcRectangleArea(base, height){
-  let rectangleArea= (base*height);
-    return rectangleArea;
+function calcRectangleArea(base, height) {
+  let rectangleArea = (base * height);
+  return rectangleArea;
 
 }
 calcRectangleArea();
 
-function calcAllAreas(base, height, form){
-  if(form === 'triângulo'){
-    return `O valor da área do triângulo é de: ${calcTriangleArea(base,height)}`;
+function calcAllAreas(base, height, form) {
+  if (form === 'triângulo') {
+    return `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}`;
 
-  }else if(form === 'retângulo'){
+  } else if (form === 'retângulo') {
     return `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}`;
 
-  }else{
+  } else {
     return `Não foi possível fazer o cálculo, insira uma forma geométrica válida`;
   }
 
@@ -80,41 +80,93 @@ function calcAllAreas(base, height, form){
 calcAllAreas();
 
 // Desafio 7 - Crie a função catAndMouse
-function catAndMouse(mouse, cat1, cat2){
-  let distance1= Math.abs(mouse-cat1);
-  let distance2= Math.abs(mouse-cat2);
-    if(distance1 < distance2 && distance1 !== distance2){
-        return `cat1`;
-    }else if(distance2 < distance1 && distance2 !== distance1){
-      return `cat2`;
-    }else if(distance1 == distance2){
-      return `os gatos trombam e o rato foge`;
-    }
-  
+function catAndMouse(mouse, cat1, cat2) {
+  let distance1 = Math.abs(mouse - cat1);
+  let distance2 = Math.abs(mouse - cat2);
+  if (distance1 < distance2 && distance1 !== distance2) {
+    return `cat1`;
+  } else if (distance2 < distance1 && distance2 !== distance1) {
+    return `cat2`;
+  } else if (distance1 == distance2) {
+    return `os gatos trombam e o rato foge`;
+  }
+
 
 }
 catAndMouse(10, 5, 5);
 
 // Desafio 8 - Crie a função fizzBuzz
-function fizzBuzz(listNumbers){
-  let listStrings= []
-    for(let i=0; i<listNumbers.length; i++){
-      if(listNumbers[i]% 3 !== 0 && listNumbers[i]% 5 !== 0){
-        listStrings.push('bug!');
-      }else if(listNumbers[i]% 3 == 0 && listNumbers[i]% 5 !== 0){
-        listStrings.push('fizz');
-      }else if(listNumbers[i]% 5 == 0 && listNumbers[i]% 3 !== 0){
-        listStrings.push('buzz');
-      }else if(listNumbers[i]% 3 == 0 && listNumbers[i]% 5 == 0){
-        listStrings.push('fizzBuzz');
-      }
+function fizzBuzz(listNumbers) {
+  let listStrings = []
+  for (let i = 0; i < listNumbers.length; i++) {
+    if (listNumbers[i] % 3 !== 0 && listNumbers[i] % 5 !== 0) {
+      listStrings.push('bug!');
+    } else if (listNumbers[i] % 3 == 0 && listNumbers[i] % 5 !== 0) {
+      listStrings.push('fizz');
+    } else if (listNumbers[i] % 5 == 0 && listNumbers[i] % 3 !== 0) {
+      listStrings.push('buzz');
+    } else if (listNumbers[i] % 3 == 0 && listNumbers[i] % 5 == 0) {
+      listStrings.push('fizzBuzz');
     }
-      return listStrings;
+  }
+  return listStrings;
 }
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
+fizzBuzz([2, 15, 7, 9, 45]);
 
 // Desafio 9 - Crie a função encode e a função decode
+function encode(word) {
+  let newWord = ''
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === 'a') {
+      let newLetter = word[i].replace('a', '1');
+      newWord += newLetter;
+    } else if (word[i] === 'e') {
+      let newLetter = word[i].replace('e', '2');
+      newWord += newLetter;
+    } else if (word[i] === 'i') {
+      let newLetter = word[i].replace('i', '3');
+      newWord += newLetter;
+    } else if (word[i] === 'o') {
+      word[i].replace('o', '4');
+    } else if (word[i] === 'u') {
+      let newLetter = word[i].replace('u', '5');
+      newWord += newLetter;
+    } else if (word[i] === ' ') {
+      newWord += ' ';
+    }else{
+      newWord+=word[i];
+    }
+  }
+  return newWord;
+}
+encode('hi there!');
 
+function decode(word){
+  let decodeWord = ''
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === '1') {
+      let newLetter = word[i].replace('1', 'a');
+      decodeWord += newLetter;
+    } else if (word[i] === '2') {
+      let newLetter = word[i].replace('2', 'e');
+      decodeWord += newLetter;
+    } else if (word[i] === '3') {
+      let newLetter = word[i].replace('3', 'i');
+      decodeWord += newLetter;
+    } else if (word[i] === '4') {
+      word[i].replace('4', 'o');
+    } else if (word[i] === '5') {
+      let newLetter = word[i].replace('5', 'u');
+      decodeWord += newLetter;
+    } else if (word[i] === ' ') {
+      decodeWord += ' ';
+    }else{
+      decodeWord+=word[i];
+    }
+  }
+  return decodeWord;
+}
+decode('h3 th2r2!');
 // Desafio 10 - Crie a função techList
 
 // Não modifique essas linhas
